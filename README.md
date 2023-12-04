984e33f072bf6671e9857c08f2f1459087ed9a5a
Benja Appel <78376862+Lyqst@users.noreply.github.com>
2023-12-04 13:10:53 +0000

    day 4

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: 72.206ms
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.617ms
+Time part 2: 3.185ms
+Both parts: 4.803ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 79.149ms
+Total stars: 8/50
+Total time: 83.951ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.js
@@ -0,0 +1,68 @@
+import run from "aocrunner";
+
+const part1 = (input) => {
+  const sum = input.split('\n').reduce((total, card) => {
+    const [leftList, rightList] = card.split(/[:][ ]+/)[1].split(/[ ]+[|][ ]+/);
+    const leftArray = leftList.split(/[ ]+/);
+    const rightArray = rightList.split(/[ ]+/);
+    const winningNumbers = leftArray.filter((number) => rightArray.includes(number)).length;
+    return winningNumbers > 0 ? total + Math.pow(2, parseInt(winningNumbers) - 1) : total;
+  }, 0);
+  return sum;
+};
+
+const part2 = (input) => {
+  const cards = input.split('\n');
+  let instances = Array.from({ length: cards.length }, () => 1);
+  cards.forEach((card) => {
+    const [tag, sides] = card.split(/[:][ ]+/);
+    const cardNumber = parseInt(tag.split(/[ ]+/)[1]);
+    const [leftList, rightList] = sides.split(/[ ]+[|][ ]+/);
+    const leftArray = leftList.split(/[ ]+/);
+    const rightArray = rightList.split(/[ ]+/);
+    const winningNumbers = leftArray.filter((number) => rightArray.includes(number)).length;
+    for (let i = 0; i < winningNumbers; i++) {
+      if(cardNumber + i < instances.length) {
+        instances[cardNumber + i] = instances[cardNumber + i] + instances[cardNumber - 1];
+      }
+    }
+  });
+  return instances.reduce((total, instance) => total + instance, 0);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+          Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+          Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        `,
+        expected: 13,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+          Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+          Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        `,
+        expected: 30,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  // onlyTests: true,
+});
